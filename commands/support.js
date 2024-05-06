5c1d8159472d4ea91861311fd98245e59e89c9db
rdean3107 <162683228+rdean3107@users.noreply.github.com>
2024-05-06 18:22:56 +0700

    Update support.js

--- commands/support.js
+++ commands/support.js
@@ -40,7 +40,7 @@ module.exports = {
               iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1230824519220985896/6280-2.gif?ex=6638ae28&is=66375ca8&hm=13e4a1b91a95b2934a39de1876e66c11711c7b30ac1a91c2a158f2f2ed1c2fc6&', 
               url: 'https://discord.gg/xQF9f9yUEM'
           })
-            .setDescription(`➡️ **Join our Discord server for support and updates:**\n- Discord - ${supportServerLink}\n\n➡️ **Follow us on:**\n- GitHub - ${githubLink}\n- Replit - ${replitLink}\n- YouTube - ${youtubeLink}`)
+            .setDescription(`➡️ **Join our Discord server for support and updates:**\n- Discord - ${supportServerLink}\n\n➡️ **Follow us on:**\n- Instagram - ${instagramLink}\n- Replit - ${facebookLink}\n- YouTube - ${discordLink}`)
             .setImage('https://cdn.discordapp.com/attachments/1211355355079254138/1215148642659467336/a08282e9e8894228a74a00b7fdc2de01_00_00_00-00_00_02.gif?ex=6639a8dc&is=6638575c&hm=3545db8ef258d3e226480362c30801da470308154633fe0b12c8288eeba73c2f&')
             .setTimestamp();
 
